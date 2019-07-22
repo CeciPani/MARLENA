94e60f82438240fb076c757fc8d51a4e7331feeb
CeciPani <cecilia.panigutti@gmail.com>
2019-07-22 10:11:13 +0200

    marlena

---
+++ marlena/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='marlena',
+      version='0.2',
+      description='It explains multi-label black boxes decisions for tabular data',
+      url='https://github.com/CeciPani/MARLENA',
+      author='Cecilia Panigutti, Riccardo Guidotti, Anna Monreale',
+      author_email='cecilia.panigutti@sns.it',
+      license='',
+      packages=['marlena'],
+      zip_safe=False)
+
